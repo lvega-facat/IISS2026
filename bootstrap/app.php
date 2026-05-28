2cc56fc02f335fe66c2d772e011ad07479bdcc5f
jesusbaez41-rgb <jesus.baez41@unae.edu.py>
2026-05-28 11:17:42 -0300

    Ajustes de Contenedor Docker y proyecto listo para editarse

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,6 +9,14 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+            then: function () {
+
+        if (app()->environment('local')) {
+            Route::middleware('web')
+                ->group(base_path('routes/dev.php'));
+        }
+
+    },
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
