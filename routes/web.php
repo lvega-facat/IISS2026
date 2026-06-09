349e58ed3153b0cf97016cf686ef9aa3614c0c53
jesusbaez41-rgb <jesus.baez41@unae.edu.py>
2026-06-09 14:17:37 -0300

    HU Usuarios

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Modules\Usuarios\Controllers\UsuariosController;
 
 Route::get('/', function () {
     return view('welcome');
-})->middleware('permission:dashboard.ver');
+})->middleware('auth');
 
     Route::get('/login', [LoginController::class, 'show'])->name('login');
     Route::post('/login', [LoginController::class, 'store'])->name('login.store');
