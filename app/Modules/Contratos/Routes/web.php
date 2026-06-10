6ed3272bbcbe161ee4ae1297753176ca1af26a85
jesusbaez41-rgb <jesus.baez41@unae.edu.py>
2026-06-10 19:49:05 -0300

    Arreglo de Profesiones

--- app/Modules/Contratos/Routes/web.php
+++ app/Modules/Contratos/Routes/web.php
@@ -15,15 +15,25 @@ Route::prefix('contratos')->middleware(['auth'])->group(function () {
 });
 
 //rutas para profesiones
-Route::prefix('contratos/profesiones')->middleware(['auth'])->group(function () {
-    Route::get('/', [ProfesionesController::class, 'index'])->name('profesiones.index')->middleware('permisos:' . Permisos::CONTRATOS_VER);
-    Route::get('/crear', [ProfesionesController::class, 'create'])->name('profesiones.create')->middleware('permisos:' . Permisos::CONTRATOS_CREAR);
-    Route::post('/', [ProfesionesController::class, 'store'])->name('profesiones.store')->middleware('permisos:' . Permisos::CONTRATOS_CREAR);
-    Route::get('/{id}', [ProfesionesController::class, 'show'])->name('profesiones.show')->middleware('permisos:' . Permisos::CONTRATOS_VER);
-    Route::get('/{id}/editar', [ProfesionesController::class, 'edit'])->name('profesiones.edit')->middleware('permisos:' . Permisos::CONTRATOS_EDITAR);
-    Route::put('/{id}', [ProfesionesController::class, 'update'])->name('profesiones.update')->middleware('permisos:' . Permisos::CONTRATOS_EDITAR);
-    Route::patch('/{id}/desactivar', [ProfesionesController::class, 'destroy'])->name('profesiones.destroy')->middleware('permisos:' . Permisos::CONTRATOS_ELIMINAR);
-});
+
+Route::prefix('contratos/profesiones')
+    ->middleware(['auth'])
+    ->group(function () {
+        Route::get('/', [ProfesionesController::class, 'index'])
+            ->name('index')->middleware('permisos:' . Permisos::CONTRATOS_VER);
+        Route::get('/crear', [ProfesionesController::class, 'create'])
+            ->name('create')->middleware('permisos:' . Permisos::CONTRATOS_CREAR);
+        Route::post('/', [ProfesionesController::class, 'store'])
+            ->name('store')->middleware('permisos:' . Permisos::CONTRATOS_CREAR);
+        Route::get('/{id}', [ProfesionesController::class, 'show'])
+            ->name('show')->middleware('permisos:' . Permisos::CONTRATOS_VER);
+        Route::get('/{id}/editar', [ProfesionesController::class, 'edit'])
+            ->name('edit')->middleware('permisos:' . Permisos::CONTRATOS_EDITAR);
+        Route::put('/{id}', [ProfesionesController::class, 'update'])
+            ->name('update')->middleware('permisos:' . Permisos::CONTRATOS_EDITAR);
+        Route::patch('/{id}/desactivar', [ProfesionesController::class, 'destroy'])
+            ->name('destroy')->middleware('permisos:' . Permisos::CONTRATOS_ELIMINAR);
+    });
 
 
 
