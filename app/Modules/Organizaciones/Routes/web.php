224f8186783e8ea186158705e7fca8f22c3284ec
jesusbaez41-rgb <jesus.baez41@unae.edu.py>
2026-06-13 12:15:13 -0300

    Arreglo rutas

--- app/Modules/Organizaciones/Routes/web.php
+++ app/Modules/Organizaciones/Routes/web.php
@@ -1,15 +1,15 @@
 <?php
 
-use App\Modules\Organizaciones\Controllers\OrganizacionController;
+use App\Modules\Organizaciones\Controllers\OrganizacionesController;
 use Illuminate\Support\Facades\Route;
 use App\Modules\RolesPermisos\Enums\Permisos;
 
 Route::prefix('organizacion')->middleware(['auth'])->group(function () {
-    Route::get('/', [OrganizacionController::class, 'index'])->middleware('Permisos:' . Permisos::ORGANIZACION_VER);
-        Route::get('/create', [OrganizacionController::class, 'create'])->middleware('Permisos:' . Permisos::ORGANIZACION_CREAR);
-        Route::get('/{id}/edit', [OrganizacionController::class, 'edit'])->middleware('Permisos:' . Permisos::ORGANIZACION_EDITAR);
-        Route::post('/', [OrganizacionController::class, 'store'])->middleware('Permisos:' . Permisos::ORGANIZACION_CREAR);
-        Route::put('/{id}', [OrganizacionController::class, 'update'])->middleware('Permisos:' . Permisos::ORGANIZACION_EDITAR);
-        Route::delete('/{id}', [OrganizacionController::class, 'destroy'])->middleware('Permisos:' . Permisos::ORGANIZACION_ELIMINAR);
+    Route::get('/', [OrganizacionesController::class, 'index'])->middleware('Permisos:' . Permisos::ORGANIZACION_VER);
+        Route::get('/create', [OrganizacionesController::class, 'create'])->middleware('Permisos:' . Permisos::ORGANIZACION_CREAR);
+        Route::get('/{id}/edit', [OrganizacionesController::class, 'edit'])->middleware('Permisos:' . Permisos::ORGANIZACION_EDITAR);
+        Route::post('/', [OrganizacionesController::class, 'store'])->middleware('Permisos:' . Permisos::ORGANIZACION_CREAR);
+        Route::put('/{id}', [OrganizacionesController::class, 'update'])->middleware('Permisos:' . Permisos::ORGANIZACION_EDITAR);
+        Route::delete('/{id}', [OrganizacionesController::class, 'destroy'])->middleware('Permisos:' . Permisos::ORGANIZACION_ELIMINAR);
 });
 //
